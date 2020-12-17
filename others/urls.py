b73a7e8fcaadd77d8779dde5353b7beeab90ce9b
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2020-12-18 01:17:27 +0800

    Added "our client" page

--- others/urls.py
+++ others/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from .views import home, load_static, about_us
+from .views import home, load_static, about_us, our_client, link, contact_us, join_us
 
 
 app_name = "others"
 
 urlpatterns = [
     path("about-us", about_us, name="about_us"),
-    # path("our_client", our_client, name="our-client"),
-    # path("link", link, name="link"),
-    # path("contact-us", contact_us, name="contact-us"),
-    # path('join-us', join_us, name="join-us"),
+    path("our_client", our_client, name="our_client"),
+    path("link", link, name="link"),
+    path("contact-us", contact_us, name="contact_us"),
+    path('join-us', join_us, name="join_us"),
     path("static/<path:filepath>", load_static, name="static"),
     path("", home, name="home"),
 ]
